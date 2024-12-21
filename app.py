90e85cd4ca00745cc0374eb5e9b5305e97061444
Meefx <muhfakhri35@gmail.com>
2024-12-21 21:18:24 +0700

    feat: get_features

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import asyncio
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 
 from finsearch.api import HealthCheckResponse
 
@@ -11,3 +12,14 @@ async def health_check():
     return HealthCheckResponse(
         status="running",
     )
+
+@app.get("/features")
+def get_features():
+    return JSONResponse(content={
+        "status": 200,
+        "data": [
+            "BM25",
+            "ColBERT",
+            "BM25 + Davinci OpenAI"
+        ]
+    })
