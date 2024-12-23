54dffc707c997d7c577aec9560bb1b7558b4111f
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-12-23 17:09:26 +0700

    feat: config for BM25

--- finsearch/retrieval/config.py
+++ finsearch/retrieval/config.py
@@ -7,3 +7,11 @@ class ColBERTConfig:
     index_name: str = "ir"
     index_url: str = "https://drive.google.com/uc?&id=1Y1Eee4mHQ-jDvBUNehFfEmMj-r9iNDcf"
     folder_name: str = "experiments"
+
+@dataclass
+class BM25Config:
+    experiment_path: str = "/app/index/experiments/bm25"
+    index_name: str = "ir"
+    bsbi_name: str = "bsbi.py" 
+    index_url: str = "https://drive.google.com/drive/folders/1Kv1uvEI508BaIOdhSEwikqJ5ubPkd-DY"
+    folder_name: str = "experiments"
