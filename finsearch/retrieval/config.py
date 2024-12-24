a9ce428b5d7b268481c71692a6012a88a785e1f9
Ghana Ahmada Yudistira <ghana.ahmada@ui.ac.id>
2024-12-24 19:55:13 +0700

    feat: move path to config

--- finsearch/retrieval/config.py
+++ finsearch/retrieval/config.py
@@ -5,14 +5,21 @@ from dataclasses import dataclass
 class ColBERTConfig:
     experiment_path: str = "/app/index/roberta_experiments/msmarco"
     index_name: str = "ir"
-    index_url: str = "https://drive.google.com/uc?&id=1KnRntVbfaUUgkWsV6GnGfTLEEq8kXTO8"
+    index_url: str = "https://drive.google.com/uc?&id=1rKzsF9BWinFJGKr3nqCp26-gK1GQxzVE"
     folder_name: str = "roberta_experiments.zip"
 
 @dataclass
 class BM25Config:
-    arxiv_collections_url: str = "https://drive.google.com/uc?&id=18o8MduiMdkBOpa--6Nere7uGQNxJohaT"
+    output_dir: str = r"app\index\bm25_index"
+    data_dir: str = r'app\index\arxiv_collections'
+    embed_openai_path: str = "app/index/document_embedded.json"
+    tfidf_model: str = "app/finsearch/retrieval/model/constant/model_tfidf.json"
+    openai_model: str = "app/finsearch/retrieval/model/constant/model_openai.json"
+
     arxiv_collections_folder: str = "arxiv_collections.zip"
-    index_url: str = "https://drive.google.com/uc?&id=1WHVMtvlLvPL9bGMCq6l-XxGjFA6Zzzyp"
-    index_folder: str = "index.zip"
-    embed_url: str = "https://drive.google.com/uc?&id=1Dbrbs1bHfbcVh9PRtxmJPQDDg9ypZTQw"
+    index_folder: str = "bm25_index.zip"
     embed_filename: str = "document_embedded.json"
+
+    index_url: str = "https://drive.google.com/uc?&id=1PDsalRBWyFL4lm5a7zGUpXmGbtkcO9DI"
+    embed_url: str = "https://drive.google.com/uc?&id=1Dbrbs1bHfbcVh9PRtxmJPQDDg9ypZTQw"
+    arxiv_collections_url: str = "https://drive.google.com/uc?&id=18o8MduiMdkBOpa--6Nere7uGQNxJohaT"
