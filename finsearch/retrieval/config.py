587b6d51cd1dffeb5be8dc062c6cd8f5a07aa37a
Ghana Ahmada Yudistira <ghana.ahmada@ui.ac.id>
2024-12-24 17:43:05 +0700

    feat: fix path

--- finsearch/retrieval/config.py
+++ finsearch/retrieval/config.py
@@ -6,15 +6,13 @@ class ColBERTConfig:
     experiment_path: str = "/app/index/roberta_experiments/msmarco"
     index_name: str = "ir"
     index_url: str = "https://drive.google.com/uc?&id=1KnRntVbfaUUgkWsV6GnGfTLEEq8kXTO8"
-    folder_name: str = "roberta_experiments"
+    folder_name: str = "roberta_experiments.zip"
 
 @dataclass
 class BM25Config:
-    experiment_path: str = "/app/index/experiments/bm25"
     arxiv_collections_url: str = "https://drive.google.com/uc?&id=18o8MduiMdkBOpa--6Nere7uGQNxJohaT"
-    arxiv_collections_folder: str = "arxiv_collections"
+    arxiv_collections_folder: str = "arxiv_collections.zip"
     index_url: str = "https://drive.google.com/uc?&id=1WHVMtvlLvPL9bGMCq6l-XxGjFA6Zzzyp"
-    index_folder: str = "index"
+    index_folder: str = "index.zip"
     embed_url: str = "https://drive.google.com/uc?&id=1Dbrbs1bHfbcVh9PRtxmJPQDDg9ypZTQw"
     embed_filename: str = "document_embedded.json"
-    embed_folder: str = "experiment/data"
